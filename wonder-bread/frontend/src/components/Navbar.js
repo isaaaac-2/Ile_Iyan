1a2001ca12a6f4f497cdb0d1ab004ce3f93e8481
Vortex-404 <isaacakinyemiisatrun@gmail.com>
2026-02-18 14:17:10 +0100

    okayyyyyyyyy

--- wonder-bread/frontend/src/components/Navbar.js
+++ wonder-bread/frontend/src/components/Navbar.js
@@ -38,14 +38,22 @@ const Navbar = ({ currentPage, onNavigate }) => {
               <button
                 onClick={() => onNavigate("landing")}
                 className={`wb-nav-link ${isActive("landing")}`}
-                style={{ background: "none", border: "none", cursor: "pointer" }}
+                style={{
+                  background: "none",
+                  border: "none",
+                  cursor: "pointer",
+                }}
               >
                 Home
               </button>
               <button
                 onClick={() => onNavigate("menu")}
                 className={`wb-nav-link ${isActive("menu")}`}
-                style={{ background: "none", border: "none", cursor: "pointer" }}
+                style={{
+                  background: "none",
+                  border: "none",
+                  cursor: "pointer",
+                }}
               >
                 Menu
               </button>
